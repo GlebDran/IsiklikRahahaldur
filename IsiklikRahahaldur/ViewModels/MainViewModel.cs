8c36365856c944a4b242aeb82fda1cad7fa1257e
Gleb Dranitsyn <glebdranitsyn@gmail.com>
2025-10-21 16:36:32 +0300

    category upd+

--- IsiklikRahahaldur/ViewModels/MainViewModel.cs
+++ IsiklikRahahaldur/ViewModels/MainViewModel.cs
@@ -5,55 +5,68 @@ using IsiklikRahahaldur.Views;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
-using Transaction = IsiklikRahahaldur.Models.Transaction;
 
-namespace IsiklikRahahaldur.ViewModels;
-
-// Мы убрали IRecipient, т.к. больше не получаем сообщения
-public partial class MainViewModel : BaseViewModel
+namespace IsiklikRahahaldur.ViewModels
 {
-    private readonly DatabaseService _databaseService;
-
-    [ObservableProperty]
-    private ObservableCollection<Transaction> _transactions;
-
-    [ObservableProperty]
-    private decimal _balance;
-
-    public MainViewModel(DatabaseService databaseService)
+    public partial class MainViewModel : BaseViewModel
     {
-        _databaseService = databaseService;
-        Title = "Мой кошелек";
-        Transactions = new ObservableCollection<Transaction>();
-    }
+        private readonly DatabaseService _databaseService;
 
-    [RelayCommand]
-    private async Task LoadTransactionsAsync()
-    {
-        var transactionsFromDb = await _databaseService.GetTransactionsAsync();
-        Transactions.Clear();
-        foreach (var t in transactionsFromDb.OrderByDescending(x => x.Date)) // Сортируем по дате
+        // Теперь коллекция хранит нашу новую "обертку"
+        [ObservableProperty]
+        private ObservableCollection<TransactionDisplayViewModel> _transactions;
+
+        [ObservableProperty]
+        private decimal _balance;
+
+        public MainViewModel(DatabaseService databaseService)
         {
-            Transactions.Add(t);
+            _databaseService = databaseService;
+            Title = "Мой кошелек";
+            Transactions = new ObservableCollection<TransactionDisplayViewModel>();
         }
-        CalculateBalance();
-    }
 
-    private void CalculateBalance()
-    {
-        decimal totalIncome = Transactions.Where(t => t.IsIncome).Sum(t => t.Amount);
-        decimal totalExpense = Transactions.Where(t => !t.IsIncome).Sum(t => t.Amount);
-        Balance = totalIncome - totalExpense;
-    }
-
-    [RelayCommand]
-    private async Task GoToAddTransactionAsync(bool isIncome)
-    {
-        var navigationParameter = new Dictionary<string, object>
+        [RelayCommand]
+        private async Task LoadTransactionsAsync()
         {
-            { "IsIncome", isIncome }
-        };
-        await Shell.Current.GoToAsync(nameof(AddTransactionPage), navigationParameter);
+            // Сначала загружаем все транзакции и категории
+            var transactionsFromDb = await _databaseService.GetTransactionsAsync();
+            var categoriesFromDb = await _databaseService.GetCategoriesAsync();
+
+            // Превращаем список категорий в словарь для быстрого поиска
+            var categoriesDict = categoriesFromDb.ToDictionary(c => c.Id, c => c.Name);
+
+            Transactions.Clear();
+
+            // "Склеиваем" транзакции с именами их категорий
+            foreach (var t in transactionsFromDb.OrderByDescending(x => x.Date))
+            {
+                Transactions.Add(new TransactionDisplayViewModel
+                {
+                    Transaction = t,
+                    CategoryName = categoriesDict.TryGetValue(t.CategoryId, out var name) ? name : "Без категории"
+                });
+            }
+            CalculateBalance();
+        }
+
+        private void CalculateBalance()
+        {
+            // Логика расчета баланса теперь использует вложенный объект Transaction
+            decimal totalIncome = Transactions.Where(t => t.Transaction.IsIncome).Sum(t => t.Transaction.Amount);
+            decimal totalExpense = Transactions.Where(t => !t.Transaction.IsIncome).Sum(t => t.Transaction.Amount);
+            Balance = totalIncome - totalExpense;
+        }
+
+        [RelayCommand]
+        private async Task GoToAddTransactionAsync(bool isIncome)
+        {
+            var navigationParameter = new Dictionary<string, object>
+            {
+                { "IsIncome", isIncome }
+            };
+            await Shell.Current.GoToAsync(nameof(AddTransactionPage), navigationParameter);
+        }
     }
 }
 
---
+++ IsiklikRahahaldur/ViewModels/TransactionDisplayViewModel.cs
@@ -0,0 +1,11 @@
+﻿using IsiklikRahahaldur.Models;
+
+namespace IsiklikRahahaldur.ViewModels
+{
+    // Этот класс будет использоваться для отображения в списке
+    public class TransactionDisplayViewModel
+    {
+        public Transaction Transaction { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
